ac173d1010c4a1ef56a23adaff0f5b1e53aa9925
Jöran Malek <joeran3@gmail.com>
2017-04-18 11:35:20 +0200

    Parse id member

--- src/main/java/org/nuxeo/onedrive/client/OneDriveDrive.java
+++ src/main/java/org/nuxeo/onedrive/client/OneDriveDrive.java
@@ -80,6 +80,7 @@ public class OneDriveDrive extends OneDriveResource implements Iterable<OneDrive
         private Long total;
         private Long used;
         private Long remaining;
+        private String id;
 
         public Metadata(final JsonObject json) {
             super(json);
@@ -96,7 +97,9 @@ public class OneDriveDrive extends OneDriveResource implements Iterable<OneDrive
             try {
                 JsonValue value = member.getValue();
                 String memberName = member.getName();
-                if ("quota".equals(memberName)) {
+                if ("id".equals(memberName)) {
+                    id = value.asString();
+                } else if ("quota".equals(memberName)) {
                     parseMember(value.asObject(), this::parseQuotaMember);
                 }
             } catch (ParseException e) {
@@ -126,6 +129,15 @@ public class OneDriveDrive extends OneDriveResource implements Iterable<OneDrive
 
         public Long getRemaining() {
             return remaining;
+        }
+
+        @Override
+        public String getId() {
+            String id = super.getId();
+            if (id == null || id.equals(null)) {
+                id = this.id;
+            }
+            return id;
         }
     }
 }
