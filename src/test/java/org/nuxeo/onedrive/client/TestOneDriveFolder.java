4a0c6ad18f6db74351be3cd8d042875e77778a1c
Jöran <AliveDevil@users.noreply.github.com>
2020-08-05 16:55:28 +0200

    Update tests

--- src/test/java/org/nuxeo/onedrive/client/TestOneDriveFolder.java
+++ src/test/java/org/nuxeo/onedrive/client/TestOneDriveFolder.java
@@ -1,35 +1,34 @@
 /*
  * (C) Copyright 2016 Nuxeo SA (http://nuxeo.com/) and others.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *  
+ *
  * Contributors:
  *     Kevin Leturc
  */
 package org.nuxeo.onedrive.client;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+import org.nuxeo.onedrive.client.types.Drive;
+import org.nuxeo.onedrive.client.types.DriveItem;
 import org.powermock.core.classloader.annotations.PrepareForTest;
+
+import static org.junit.Assert.*;
 
 /**
  * @since 1.0
  */
-@PrepareForTest(OneDriveFolder.class)
+@PrepareForTest(DriveItem.class)
 public class TestOneDriveFolder extends OneDriveTestCase {
 
     @Test
@@ -38,9 +37,9 @@ public class TestOneDriveFolder extends OneDriveTestCase {
         mockJsonRequest("onedrive_folder.json");
 
         // Test
-        OneDriveDrive drive = new OneDriveDrive(api, "5FGB3N6V49HN725573R8LG588VRDPMJV36");
-        OneDriveFolder folder = drive.getRoot();
-        OneDriveFolder.Metadata metadata = folder.getMetadata();
+        Drive drive = new Drive(api, "5FGB3N6V49HN725573R8LG588VRDPMJV36");
+        DriveItem folder = new DriveItem(drive);
+        DriveItem.Metadata metadata = folder.getMetadata();
         assertTrue(metadata.isFolder());
         assertEquals("Test", metadata.getName());
         assertNotNull(metadata.getParentReference());
@@ -51,8 +50,8 @@ public class TestOneDriveFolder extends OneDriveTestCase {
         assertNull(metadata.getCreatedBy().getDevice());
         assertNotNull(metadata.getCreatedBy().getUser());
         assertEquals("Nuxeo User", metadata.getCreatedBy().getUser().getDisplayName());
-        assertEquals(3, metadata.getChildCount());
-        assertEquals(0L, metadata.getSize());
+        assertEquals(3, metadata.getFolder().getChildCount().intValue());
+        assertEquals(0L, metadata.getSize().intValue());
     }
 
 }
